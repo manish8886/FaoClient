72520b9639825389191bfd7e4d1c81c155e9c9e0
Manish <>
2012-11-07 05:07:46 -0500

    Added function for getting the data from URL for main table and update it in the database.

--- FaoClientPrj/src/FaoClient/CFaoTable.java
+++ FaoClientPrj/src/FaoClient/CFaoTable.java
@@ -1,31 +1,74 @@
-
 package FaoClient;
+import java.sql.*;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Vector;
+import org.dom4j.Document;
+import org.dom4j.Node;
 /**
  *
  * @author Manish Jain
  */
 public class CFaoTable {
+    Vector<TableCol>Columns;
     String Name;
-    String SrcTableName;
-    int iBoundColId;
-    Vector<String>ColNames;
-    String Link;
-
+    String SrcNodeXML;
+    String DataUrl;
+    SourceType Type;
+    enum SourceType{Full,Partial};
     public CFaoTable() {
         Name = "";
-        SrcTableName = "";
-        iBoundColId = -1;
-        Link = "";
+        SrcNodeXML = "";
+        DataUrl = "";
+        Columns.clear();
+        Type = SourceType.Partial; //Means the URL of the Data Source is not Complete
     }
-    enum SourceType{Full,Partial};
     void SetTableName(String str){
         Name = str;
     }
-    void SetColNames(Vector<String> colString){
-        ColNames= colString;
+    void SetColumns(Vector<TableCol> colVec){
+        Columns= colVec;
     }
-    void SetDataSourcePath(String Path){
-        Link = Path;
+    void SetDataSource(String Path){
+        DataUrl = Path;
     }
- };
+    void SetSourceType(SourceType t){
+        if(t!= Type){
+            Type = t;
+        }
+    }
+    void InsertFromFullSource(Connection conn,Document doc){
+        List listOfNode= doc.selectNodes(SrcNodeXML);
+        try {   
+        conn.setAutoCommit(false);
+        Statement  st = conn.createStatement();
+         //Here the data stored in each node will consitute our one row in 
+        // the table
+        for(Iterator iter = listOfNode.iterator(); iter.hasNext();){
+           Node rowElement = (Node) iter.next();
+           String insertStmt = "insert into "+Name+" values (";
+            for(int index=0;index<Columns.size();index++){
+            TableCol col= Columns.get(index);
+            String colVal = rowElement.selectSingleNode(col.GetColName()).getText();
+            if(col.GetColType()==TableCol.ColType.STRINGT){
+                    colVal= "'"+colVal+"'";
+            }
+            if(index != Columns.size()-1){
+                    insertStmt =  insertStmt + colVal+", ";   
+            }
+             else{
+                    insertStmt =  insertStmt + colVal+")";
+                 }
+            }
+                   st.addBatch(insertStmt);
+            }
+            st.executeBatch();
+            conn.commit();
+        }
+    catch(SQLException E){
+             System.out.println("Error in insert in to table: "+ Name + E);
+        }
+    }
+    
+}
+ 
