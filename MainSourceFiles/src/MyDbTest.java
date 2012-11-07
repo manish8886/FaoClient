242ee981d9a459ac45d6739247342091e7c01731
Manish Jain <manish8886@gmail.com>
2012-11-07 02:58:49 -0500

    External Libraries

---
+++ MainSourceFiles/src/MyDbTest.java
@@ -0,0 +1,28 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mydbtest;
+import java.sql.*;
+public class MyDbTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String dataSourceName = "testMyDB";
+        String dbURL = "jdbc:odbc:"+dataSourceName;
+        try{
+            Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+            Connection con = DriverManager.getConnection(dbURL);
+            Statement s = con.createStatement();
+            s.execute("create table school2(Grade integer,Remark text)");
+            s.close();  
+            con.close();    
+        }
+        catch(Exception E){
+            System.out.println("Kamilian"+E);   
+        }
+        }
+        
+    }
